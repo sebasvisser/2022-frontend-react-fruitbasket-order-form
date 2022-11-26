139738eba3cf65b2fb33aece09eaa5e5f66e3dbf
sebasvisser <sebas.visser@mensenrelatie.nl>
2022-11-27 00:42:56 +0100

    ternary zodat niet onder 0 kan

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -11,7 +11,7 @@ function Product({produce, emoticon}) {
             <div className={"fruit-block"}>
                 <span>{emoticon}</span>
                 <h2>{produce}</h2>
-                <button onClick={() => setCount(count - 1)}>MIN</button>
+                <button onClick={() => count === 0 ? setCount(count) : setCount(count - 1)}>MIN</button>
                 <h3>{count}</h3>
                 <button onClick={() => setCount(count + 1)}>PLUS</button>
             </div>
